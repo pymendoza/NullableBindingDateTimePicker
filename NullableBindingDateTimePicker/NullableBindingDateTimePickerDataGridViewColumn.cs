a308a55d6652d34ca35572748a61b0ebf30e2208
Perry Mendoza <perry.mendoza@stickytickets.com.au>
2019-06-02 04:26:43 +0800

    Added implementation for nullable binding datetimepicker on datagridviewcolumn

---
+++ NullableBindingDateTimePicker/NullableBindingDateTimePickerDataGridViewColumn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NullableBindingDateTimePicker
+{
+    /// <summary>
+    /// Based on https://docs.microsoft.com/en-us/dotnet/framework/winforms/controls/how-to-host-controls-in-windows-forms-datagridview-cells
+    /// </summary>
+    public class NullableBindingDateTimePickerDataGridViewColumn : DataGridViewColumn
+    {
+        public NullableBindingDateTimePickerDataGridViewColumn()
+            : base(new DateTimePickerDataGridViewCell())
+        {
+        }
+
+        public override DataGridViewCell CellTemplate
+        {
+            get
+            {
+                return base.CellTemplate;
+            }
+            set
+            {
+                if (value != null && !value.GetType().IsAssignableFrom(typeof(DateTimePickerDataGridViewCell)))
+                {
+                    throw new InvalidCastException("Must be a DateTimePickerDataGridViewCell");
+                }
+
+                base.CellTemplate = value;
+            }
+        }
+    }
+}
